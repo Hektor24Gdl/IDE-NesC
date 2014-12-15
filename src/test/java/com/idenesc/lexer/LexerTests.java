caf7ffe2a32db52b8375abe5b0386b52127b6815
Gibran Felix Zavala <gibranfelix@gmail.com>
2014-12-15 10:58:19 -0600

    Agregar el throws LexerError en LexerTests

--- src/test/java/com/idenesc/lexer/LexerTests.java
+++ src/test/java/com/idenesc/lexer/LexerTests.java
@@ -1,6 +1,7 @@
 package com.idenesc.lexer;
 
 import code.Lexer;
+import exceptions.LexerError;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -26,36 +27,36 @@ public class LexerTests {
     }
 
     @Test(groups = {s})
-    public void success() throws IOException {
+    public void success() throws IOException, LexerError {
         reloadFile(LexerTests.class.getResourceAsStream("/avroraz/test.txt"));
         iterateTokens();
     }
 
     @Test(groups = {e}, expectedExceptions = Error.class, description = "Identificar inválido. int 1v1 = 5;")
-    public void invalidIdentifier() throws IOException {
+    public void invalidIdentifier() throws IOException, LexerError {
         reloadFile(LexerTests.class.getResourceAsStream("/avroraz/invalidId.txt"));
         iterateTokens();
     }
     
     @Test(groups = {e}, expectedExceptions = Error.class, description = "Suma incorrecta. v2 = v1++v1;")
-    public void invalidVariable() throws IOException {
+    public void invalidVariable() throws IOException, LexerError {
         reloadFile(LexerTests.class.getResourceAsStream("/avroraz/invalidVariable.txt"));
         iterateTokens();
     }
     
     @Test(groups = {e}, expectedExceptions = Error.class, description = "Tipo de dato incorrecto. 1int v2;")
-    public void invalidType() throws IOException {
+    public void invalidType() throws IOException, LexerError {
         reloadFile(LexerTests.class.getResourceAsStream("/avroraz/invalidType.txt"));
         iterateTokens();
     }
 
     @Test(groups = {e}, expectedExceptions = Error.class, description = "Corchete sin cierre. int[ v2;")
-    public void invalidSquareBrackets() throws IOException {
+    public void invalidSquareBrackets() throws IOException, LexerError {
         reloadFile(LexerTests.class.getResourceAsStream("/avroraz/invalidSquareBrackets.txt"));
         iterateTokens();
     }
     
-    public void iterateTokens() throws IOException{
+    public void iterateTokens() throws LexerError, IOException {
         while(lexer.next_token().sym > 0){}
     }
     
